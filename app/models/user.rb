aaa5c09fccd5d4c7f00d7b66d00caa675b19030a
Emil Soman <emil.soman@gmail.com>
2013-03-30 18:50:14 +0530

    Add omniauth provider in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :omniauthable
+  devise :omniauthable, :omniauth_providers => [:facebook]
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :remember_me, :provider, :uid
