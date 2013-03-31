98928fcd1411257243bda2de3e8730b0507680f6
Emil Soman <emil.soman@gmail.com>
2013-03-31 21:39:18 +0530

    Login with Facebook AND Github

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,23 +1,20 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
-    set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-    if current_user
-      redirect_to 'authentication#home'
-    else
-      sign_in_and_redirect user, :event => :authentication #this will throw if @user is not activated
-    end
+    sign_in_user_and_redirect
   end
 
   def github
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    user = User.find_for_github_oauth(request.env["omniauth.auth"], current_user)
-    set_flash_message(:notice, :success, :kind => "GitHub") if is_navigational_format?
-    if current_user
-      redirect_to 'authentication#home'
-    else
-      sign_in_and_redirect user, :event => :authentication #this will throw if @user is not activated
-    end
+    sign_in_user_and_redirect
   end
+
+  private
+    def sign_in_user_and_redirect
+      user = User.find_or_create_auth(request.env["omniauth.auth"], current_user)
+      #set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      if current_user
+        redirect_to 'authentication#home'
+      else
+        sign_in_and_redirect user, :event => :authentication #this will throw if @user is not activated
+      end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,25 +10,15 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :name, :password, :password_confirmation
 
-  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+  def self.find_or_create_auth(auth, current_user)
     authentication = Authentication.where(provider: auth.provider, uid: auth.uid).first
     if authentication
-      user = authentication.user
+      user = current_user || authentication.user
     else
-      user = User.create(name:auth.extra.raw_info.name, email: auth.info.email)
+      user = current_user || User.create(name:auth.extra.raw_info.name, email: auth.info.email)
       auth = Authentication.create(user_id: user.id, provider: auth.provider, uid: auth.uid)
     end
     user
   end
 
-  def self.find_for_github_oauth(auth, signed_in_resource=nil)
-    authentication = Authentication.where(provider: auth.provider, uid: auth.uid).first
-    if authentication
-      user = authentication.user
-    else
-      user = User.create(name:auth.extra.raw_info.name, email: auth.info.email)
-      auth = Authentication.create(user_id: user.id, provider: auth.provider, uid: auth.uid)
-    end
-    user
-  end
 end
